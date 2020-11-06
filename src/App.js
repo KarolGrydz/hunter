026494c5fbae3e093d85b03a22f729d53d103249
Karol <grydzkarol@gmail.com>
2020-11-06 16:10:44 +0100

    INIT APP

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import { Navbar } from './components/layout/Navbar';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <h1>Hunter app</h1>
+      <h3>nowa apka</h3>
+    </div>
+  );
+}
+
+export default App;
