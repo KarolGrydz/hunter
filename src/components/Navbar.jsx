395dbb6fc00773c85ede3983eb89a3ab1bb999ed
Karol <grydzkarol@gmail.com>
2020-11-21 18:13:10 +0100

    NAVBAR

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Avatar from '@material-ui/core/Avatar';
+
+import HunterLogo from '../assets/img/logo.jpg';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    flexGrow: 1,
+    borderBottom: `1px solid ${theme.palette.primary.dark}`,
+    fontWeight: 600,
+  },
+  appNav: {
+    flexGrow: 1,
+    justifyContent: 'flex-end',
+    display: 'flex',
+    '& a': {
+      marginLeft: theme.spacing(2),
+      '&:hover': {
+        borderBottom: `1px solid ${theme.palette.secondary.dark}`,
+      },
+    },
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar
+        position="static"
+        color="primary"
+        elevation={0}
+        className={classes.appBar}
+      >
+        <Toolbar className={classes.toolbar}>
+          <Avatar alt="Logo" src={HunterLogo} className={classes.large} />
+          <nav className={classes.appNav}>
+            <Link
+              variant="button"
+              to="/"
+            >
+              Hunter
+            </Link>
+            <Link
+              variant="button"
+              to="/kim-jestesmy/"
+            >
+              Kim jesteśmy?
+            </Link>
+            <Link
+              variant="button"
+              to="/wyprawy/"
+            >
+              Wyprawy
+            </Link>
+            <Link
+              variant="button"
+              to="/przyjaciele/"
+            >
+              Przyjaciele
+            </Link>
+            <Link
+              variant="button"
+              to="/kontakt/"
+            >
+              Kontakt
+            </Link>
+          </nav>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
+import { CssBaseline } from '@material-ui/core';
 import theme from './theme';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={createMuiTheme(theme)}>
-      <App />
+      <CssBaseline>
+        <App />
+      </CssBaseline>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
