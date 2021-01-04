9125178a9f25df4b903a2243ed296716a7b52e7c
Karol <grydzkarol@gmail.com>
2021-01-04 18:01:10 +0100

    UPDATE SELECTORS && ADD MAPPER

--- src/store/actions/selectors.js
+++ src/store/actions/selectors.js
@@ -1,42 +1,23 @@
 import { createSelector } from 'reselect';
+import returnTrip from '../mappers/tripMapper';
 
-const trips = (state) => state.blog.trips;
-const singleTrip = (state) => state.blog.singleTrip;
-const sidebarTrips = (state) => state.blog.sidebarTrips;
-const pages = (state) => state.blog.pages;
-const currentPage = (state) => state.blog.currentPage;
-const tripsNumber = (state) => state.blog.tripsNumber;
-const search = (state) => state.blog.search;
-const isLoading = (state) => state.blog.isLoading;
-const error = (state) => state.blog.error;
+const getBlog = ({ blog }) => blog;
 
-export const selectTrips = createSelector(trips, (trips) => trips.map((trip) => returnTrip(trip)));
+export const getTrips = createSelector(getBlog, (trips) => trips.map((trip) => returnTrip(trip)));
 
-export const selectSingleTrip = createSelector(singleTrip, (trip) => returnTrip(trip));
+export const selectSingleTrip = createSelector(getBlog, (trip) => returnTrip(trip));
 
-export const selectSidebarTrips = createSelector(sidebarTrips, (trips) =>
-  trips.map((trip) => returnTrip(trip)),
-);
+export const selectSidebarTrips = createSelector(getBlog, (trips) => trips
+  .map((trip) => returnTrip(trip)));
 
-export const selectAllPagesNumber = createSelector(pages, (pages) => Number(pages));
+export const selectAllPagesNumber = createSelector(getBlog, (pages) => Number(pages));
 
-export const selectCurrentPage = createSelector(currentPage, (currentPage) => Number(currentPage));
+export const selectCurrentPage = createSelector(getBlog, (currentPage) => Number(currentPage));
 
-export const selectAllPostNumber = createSelector(tripsNumber, (tripsNumber) =>
-  Number(tripsNumber),
-);
+export const selectAllPostNumber = createSelector(getBlog, (tripsNumber) => Number(tripsNumber));
 
-export const selectSearch = createSelector(search, (search) => toString(search));
+export const selectSearch = createSelector(getBlog, (search) => toString(search));
 
-export const selectLoading = createSelector(isLoading, (isLoading) => isLoading);
+export const selectLoading = createSelector(getBlog, (isLoading) => isLoading);
 
-export const selectError = createSelector(error, (err) => err);
-
-const returnTrip = (trip) => {
-  return {
-    id: trip.id,
-    title: trip.title.rendered,
-    date: trip.date,
-    content: trip.content.rendered,
-  };
-};
+export const selectError = createSelector(getBlog, (err) => err);
