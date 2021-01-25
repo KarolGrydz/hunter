e848b942556c82119f061234e4dfd0e7d8247fe0
Karol <grydzkarol@gmail.com>
2021-01-25 16:12:41 +0100

    tile init

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import Map from '../components/Map';
+import Tile from '../components/Tile';
+import tileVariables from '../constants/tileVariables';
 
 const Home = () => (
   <>
     <Map />
+    <Grid container>
+      {tileVariables.map(({
+        img, title, text, id,
+      }) => (
+        <Tile text={text} img={img} title={title} key={id} />
+      ))}
+    </Grid>
   </>
 );
 
