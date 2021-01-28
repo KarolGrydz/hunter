ef73d2c6dd08e71fe0d5f756400198791f34ad56
Karol <grydzkarol@gmail.com>
2021-01-28 21:10:43 +0100

    Friends & Contact init

---
+++ src/components/Contact.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import * as R from 'ramda';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Container, Grid, Typography, Fade,
+} from '@material-ui/core';
+import { contact } from '../constants/apiUrls';
+import Preloader from './Blog/Preloader';
+import usePages from '../hooks/usePages';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '10vh 0',
+    textAlign: 'center',
+    height: '90vh',
+  },
+  title: {
+    padding: theme.spacing(3, 0),
+    borderBottom: 'solid #e4e7e8 1px',
+    color: '#000',
+    transition: 'color 200ms ease-in-out',
+    fontWeight: 'bold',
+  },
+  text: {
+    textAlign: 'left',
+  },
+}));
+
+const Contact = () => {
+  const classes = useStyles();
+  const data = usePages(contact);
+
+  if (R.isEmpty(data)) return <Preloader />;
+
+  return (
+    <Container className={classes.root}>
+      <Fade in="true" timeout={2500}>
+        <Grid container justify="center">
+          <Typography variant="h3" className={classes.title}>
+            {data.title.rendered}
+          </Typography>
+          <Typography className={classes.text}>
+            <div dangerouslySetInnerHTML={{ __html: data.content.rendered }} />
+          </Typography>
+        </Grid>
+      </Fade>
+    </Container>
+  );
+};
+
+export default Contact;
